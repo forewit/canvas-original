c731aca5de923ca7c549456cbdb70605e60301fd
Anderson <MDAnderson@samaritan.org>
2021-06-23 17:37:31 -0400

    na

---
+++ scripts/dndcanvas.js
@@ -0,0 +1,164 @@
+import { Layer } from "./layer.js";
+
+export class Dndcanvas {
+    constructor(elm) {
+        this.elm = elm;
+        //this.rect = this.elm.getBoundingClientRect();
+        this.left = 0;
+        this.top = 0;
+        this.width = 0;
+        this.height = 0;
+        this.ctx = elm.getContext("2d");
+        this.dpi = window.devicePixelRatio;
+        this.layers = [];
+        this.originx = 0;
+        this.originy = 0;
+        this.scale = 1;
+        this.UILayer = new Layer();
+        this._activeLayer = undefined;
+
+        this.resize();
+    }
+
+    get activeLayer() { return this._activeLayer; }
+    set activeLayer(newActiveLayer) {
+        for (var i = 0, len = this.layers.length; i < len; i++) {
+            if (this.layers[i].ID == newActiveLayer.ID) {
+                this._activeLayer = newActiveLayer;
+                return;
+            }
+        }
+    }
+
+    bringForward(layer) {
+        for (var i = 0, len = this.layers.length; i < len; i++) {
+            if (this.layers[i].ID == layer.ID) {
+                this.layers.splice(i, 1);
+                this.layers.push(layer);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    sendBackward(layer) {
+        for (var i = 0, len = this.layers.length; i < len; i++) {
+            if (this.layers[i].ID == layer.ID) {
+                this.layers.splice(i, 1);
+                this.layers.unshift(layer);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    addLayer(layer) {
+        for (var i = 0, len = this.layers.length; i < len; i++) {
+            if (this.layers[i].ID == layer.ID) return
+        }
+        this.layers.push(layer);
+    }
+
+    destroyLayer() {
+        for (var i = 0, len = this.layers.length; i < len; i++) {
+            if (this.layers[i].ID == layer.ID) {
+                if (this.layers[i].ID == this._activeLayer.ID) {
+                    this._activeLayer = undefined;
+                }
+                this.layers[i].destroy();
+                this.layers.splice(i, 1);
+                return true;
+            } isadmin
+        }
+        return false;
+    }
+
+    screenToCanvas(point) {
+
+        let newX = ((point.x + this.left) * this.dpi) / this.scale + this.originx;
+        let newY = ((point.y + this.top) * this.dpi) / this.scale + this.originy;
+        
+        return {
+            x: newX,
+            y: newY
+        };
+    }
+
+    translate(dx, dy) {
+        this.originx -= dx;
+        this.originy -= dy;
+        this.ctx.translate(dx, dy);
+    }
+
+    zoomOnPoint(point, zoom) {
+        this.ctx.translate(this.originx, this.originy);
+
+        this.ctx.scale(zoom, zoom);
+        this.scale *= zoom;
+
+        this.ctx.translate(-this.originx, -this.originy);
+
+
+        /*
+        // Translate so the visible origin is at the context's origin.
+        this.ctx.translate(this.originx, this.originy);
+
+        // Compute the new visible origin. Originally the mouse is at a
+        // distance mouse/scale from the corner, we want the point under
+        // the mouse to remain in the same place after the zoom, but this
+        // is at mouse/new_scale away from the corner. Therefore we need to
+        // shift the origin (coordinates of the corner) to account for this.
+        let dx = x - this.originx;
+        let dy = y - this.originy;
+
+        // Scale it (centered around the origin due to the trasnslate above).
+        this.ctx.scale(zoom, zoom);
+
+        // Offset the visible origin to it's proper position.
+        this.ctx.translate(-this.originx, -this.originy);
+        this.translate(x, y);
+
+        // Update scale and others.
+        this.scale *= zoom;
+        */
+    }
+
+    resize() {
+        // recalculate canvas size
+        let rect = this.elm.getBoundingClientRect();
+        this.dpi = window.devicePixelRatio;
+        this.left = rect.left;
+        this.top = rect.top;
+        this.width = rect.width * this.dpi;
+        this.height = rect.height * this.dpi;
+
+        // reset and re-apply canvas transforms
+        this.ctx.resetTransform()
+        this.elm.width = this.width;
+        this.elm.height = this.height;
+        this.ctx.scale(this.scale, this.scale);
+        this.ctx.translate(-this.originx, -this.originy);
+
+        console.log("RESIZE!");
+    }
+
+    render() {
+        // Use the identity matrix while clearing the canvas
+        // TODO: verify that this is ONLY clearing the screen
+        this.ctx.clearRect(
+            this.originx, this.originy,
+            this.width / this.scale, this.height / this.scale);
+
+        // draw origin point
+        this.ctx.beginPath();
+        this.ctx.ellipse(0, 0, 10, 10, 0, 0, 6.28);
+        this.ctx.rect(this.originx, this.originy, 10, 10);
+        this.ctx.stroke();
+
+        // render content layers
+        this.layers.forEach(layer => layer.render(this.ctx));
+
+        // render UI layer
+        this.UILayer.render(this.ctx);
+    }
+}
---
+++ scripts/trash/interact.js
@@ -0,0 +1,358 @@
+// TODO: Move all "screeToCanvas" responsibility to the canvas object
+
+import { Entity } from "./entity.js";
+
+// PREFERENCES
+let zoomIntensity = 0.05;
+let inertiaFriction = 0.8; // 0 = infinite friction, 1 = no friction
+let inertiaMemory = 0.2; // 0 = infinite memory, 1 = no memory
+let inertiaDropOff = 5 // in milliseconds
+let epsilon = 0.001; // replacement for 0 to prevent divide-by-zero errors
+let handleSize = 5;
+
+// STATE MANAGEMENT
+let selected = [];
+let dndcanvas = undefined;
+let isPanning = false;
+let isResizing = false;
+let isMoving = false;
+let lastPanTime;
+let lastPoint = {x:0, y:0};
+let vx = 0, vy = 0;
+let log = document.getElementById('log');
+let gestures = {};
+
+// EXPORTS-------------------------------------------
+export let interact = {
+    setTool: setTool,
+    initialize: initialize,
+    stop: stop,
+};
+
+function setTool(name) {
+    // must be initialized before calling this function...
+
+    gestures.clear();
+
+    // global keyboard shortcuts
+    keys.on('17 82', function (e) {
+        e.preventDefault();
+        alert('Prevented reload!');
+    });
+
+    switch (name) {
+        case 'select':
+            log.innerHTML = 'Select';
+            gestures.on('click tap', (x, y) => {
+                // convert to canvas coordinates
+                var point = dndcanvas.screenToCanvas({x:x, y:y});
+
+                // clear selection if shift is not being held
+                if (!keys.down[16]) clearSelection();
+
+                selectPoint(point.x, point.y);
+            });
+            gestures.on('touchDragStart mouseDragStart pinchStart', (x, y) => {
+                // convert to canvas coordinates
+                var point = dndcanvas.screenToCanvas({x:x, y:y});
+
+                mouseDragStart(point);
+            });
+            gestures.on('touchDragging mouseDragging', (x, y) => {
+                // convert to canvas coordinates
+                var point = dndcanvas.screenToCanvas({x:x, y:y});
+
+                mouseDragging(point);
+            });
+            gestures.on('touchDragEnd mouseDragEnd pinchEnd', () => {
+                mouseDragEnd();
+            });
+            gestures.on('pinching', (x, y, zoom) => {
+                // convert to canvas coordinates
+                var point = dndcanvas.screenToCanvas({x:x, y:y});
+
+                dndcanvas.zoomOnPoint(point, zoom);
+                panning(point);
+            });
+            gestures.on('wheel', (x, y, event) => {
+                // convert to canvas coordinates
+                var point = dndcanvas.screenToCanvas({x:x, y:y});
+
+                wheel(point, event);
+            });
+            console.log(gestures)
+            break;
+
+        case 'measure':
+            break;
+
+        case 'draw':
+            break;
+
+        default:
+            console.log('Invalid tool choice!');
+            break;
+    }
+}
+
+// cnvs is a Canvas object, not an element
+function initialize(cnvs) {
+    dndcanvas = cnvs;
+    gestures = new Gestures(dndcanvas.elm)
+    gestures.start();
+    keys.start()
+}
+function stop() {
+    dndcanvas = undefined;
+    gestures.stop();
+    keys.stop();
+}
+// END EXPORTS---------------------------------------
+
+
+// **************** MOUSE TRIAGE FUNCTIONS ****************
+function mouseDragStart(point) {
+    /** TODO CASES
+     * 1. dragging active handle -> resize selection
+     * 2. draging active selection -> move selection
+     * 3. dragging an unselected entity -> select entity & move selection
+     * 4. dragging no entities -> pan
+     */
+    panStart(point);
+}
+function mouseDragging(point) {
+    /** TODO CASES
+     * 1. resize selection
+     * 2. move selection
+     * 3. pan
+     */
+    if (isPanning) {
+        panning(point);
+        return;
+    } else if (isResizing) {
+
+    } else if (isMoving) {
+
+    }
+}
+function mouseDragEnd() {
+    if (isPanning) panEnd();
+    if (isResizing) resizeEnd();
+    if (isMoving) moveEnd();
+}
+// **************************************************
+
+
+
+// **************** PANNING FUNCTIONS ***************
+function panStart(point) {
+    console.log("PAN START")
+
+    isPanning = true;
+    lastPoint.x = point.x;
+    lastPoint.y = point.y;
+
+    vx = 0;
+    vy = 0;
+}
+function panning(newPoint) {
+    let dx = newPoint.x - lastPoint.x;
+    let dy = newPoint.y - lastPoint.y;
+
+    dndcanvas.translate(dx, dy);
+
+    vx = dx * inertiaMemory + vx * (1 - inertiaMemory);
+    vy = dy * inertiaMemory + vy * (1 - inertiaMemory);
+
+    lastPoint.x = newPoint.x;
+    lastPoint.y = newPoint.y;
+
+    lastPanTime = new Date();
+}
+function panEnd() {
+    console.log("PAN END")
+
+    isPanning = false;
+    let elapsed = new Date() - lastPanTime;
+
+    vx *= Math.min(1, inertiaDropOff / elapsed);
+    vy *= Math.min(1, inertiaDropOff / elapsed);
+    requestAnimationFrame(panInertia);
+}
+function panInertia() {
+    if (isPanning || (Math.abs(vx) < epsilon && Math.abs(vy) < epsilon)) return;
+    requestAnimationFrame(panInertia);
+
+    dndcanvas.translate(vx, vy);
+
+    vx *= inertiaFriction;
+    vy *= inertiaFriction;
+}
+// **************************************************
+
+
+
+// **************** ZOOMING FUNCTIONS ***************
+function wheel(point, event) {
+    let delta = event.deltaY;
+
+    // Normalize wheel to +1 or -1.
+    let direction = delta < 0 ? 1 : -1;
+
+    // Compute zoom factor.
+    let zoom = Math.exp(direction * zoomIntensity);
+
+    // zoom
+    dndcanvas.zoomOnPoint(point, zoom);
+}
+// **************************************************
+
+
+
+// ************ RESIZE HANDLE FUNCTIONS *************
+let handles = new Entity()
+
+let outerX, outerY, outerW, outerH,
+    innerX, innerY, innerW, innerH,
+    localPoint,
+    activeHandles = [];
+
+handles.render = function (ctx) {
+    let size = handleSize / dndcanvas.scale;
+
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.rotation);
+
+    ctx.beginPath();
+    ctx.rect(-this.halfw, -this.halfh, this.w, this.h);
+    ctx.rect(-size - this.halfw, -size - this.halfh, this.w + size * 2, this.h + size * 2);
+    ctx.rect(size - this.halfw, size - this.halfh, this.w - size * 2, this.h - size * 2);
+    ctx.stroke();
+
+    ctx.rotate(-this.rotation);
+    ctx.translate(-this.x, -this.y);
+}
+function getHandleIntersection(x, y) {
+    //returns [x, y] where x or y can be -1, 0, or 1. Examples:
+    //* [-1, 0] is the Left edge
+    //* [1, 1] is the bottom right corner
+    //* [] intersects but not on handle
+    //* undefined = no intersections
+    if (selected.length <= 0) return undefined;
+
+    activeHandles = [];
+    localPoint = utils.rotatePoint(handles.x, handles.y, x, y, handles.rotation);
+
+    outerX = handles.x - handles.halfw - handleSize;
+    outerY = handles.y - handles.halfh - handleSize;
+    outerW = handles.w + handleSize * 2;
+    outerH = handles.h + handleSize * 2;
+
+    // return if point is outside the outer rect
+    if (!utils.pointInRectangle(localPoint.x, localPoint.y, outerX, outerY, outerW, outerH)) return undefined;
+
+    innerX = handles.x - handles.halfw + handleSize;
+    innerY = handles.y - handles.halfh + handleSize;
+    innerW = handles.w - handleSize * 2;
+    innerH = handles.h - handleSize * 2;
+
+    // return if point is inside the inner rect
+    if (utils.pointInRectangle(localPoint.x, localPoint.y, innerX, innerY, innerW, innerH)) return activeHandles;
+
+    activeHandles = [0, 0];
+    // check left and right handles
+    if (localPoint.x <= innerX) activeHandles[0] = -1;
+    else if (localPoint.x >= innerX + innerW) activeHandles[0] = 1;
+
+    // check top and bottom handles
+    if (localPoint.y <= innerY) activeHandles[1] = -1;
+    else if (localPoint.y >= innerY + innerH) activeHandles[1] = 1;
+    return activeHandles;
+}
+function showHandles() {
+    let boundingLeft = selected[0].x,
+        boundingRight = selected[0].x,
+        boundingTop = selected[0].y,
+        boundingBottom = selected[0].y;
+
+    for (let len = selected.length, i = 0; i < len; i++) {
+        let entity = selected[i];
+
+        let angle = entity.rotation % (Math.PI);
+        if (angle > Math.PI / 2) angle = Math.PI - angle;
+
+        let halfW = (Math.sin(angle) * entity.h + Math.cos(angle) * entity.w) / 2,
+            halfH = (Math.sin(angle) * entity.w + Math.cos(angle) * entity.h) / 2;
+
+        let left = entity.x - halfW,
+            right = entity.x + halfW,
+            top = entity.y - halfH,
+            bottom = entity.y + halfH;
+
+        if (left < boundingLeft) boundingLeft = left;
+        if (right > boundingRight) boundingRight = right;
+        if (top < boundingTop) boundingTop = top;
+        if (bottom > boundingBottom) boundingBottom = bottom;
+    }
+
+    handles.w = boundingRight - boundingLeft;
+    handles.h = boundingBottom - boundingTop;
+    handles.x = boundingLeft + handles.w / 2;
+    handles.y = boundingTop + handles.h / 2;
+    handles.rotation = 0;
+
+    // show handles
+    dndcanvas.UILayer.addEntity(handles);
+}
+function hideHandles() {
+    dndcanvas.UILayer.removeEntity(handles);
+}
+function resizeStart(x, y) {
+
+}
+function resizing(x, y) {
+
+}
+function resizeEnd() {
+
+}
+// **************************************************
+
+
+
+// ************** SELECTION FUNCTIONS ***************
+function selectPoint(x, y) {
+    // check for entity intersections
+    let intersectedEntity = dndcanvas.activeLayer.getFirstIntersection(x, y);
+    if (!intersectedEntity) return;
+
+    // check for duplicate selections
+    for (let len = selected.length, i = 0; i < len; i++) {
+        if (selected[i].ID == intersectedEntity.ID) {
+            return;
+        }
+    }
+
+    // Add intersectedEntity to selected[]
+    selected.push(intersectedEntity);
+    showHandles();
+
+    //TEMP CODE -------------------------------
+    let activeHandle = getHandleIntersection(x, y);
+    console.log(activeHandle, selected);
+    //END TEMP CODE ---------------------------
+}
+function clearSelection() {
+    selected.length = 0;
+    hideHandles();
+}
+function moveStart(x, y) {
+    isMoving = true;
+}
+function moving(x, y) {
+
+}
+function moveEnd() {
+    isMoving = false;
+}
+// **************************************************
