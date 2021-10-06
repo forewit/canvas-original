56209d228a7432a20007ef7ad9ce50cee8ede1fb
Anderson <MDAnderson@samaritan.org>
2021-10-06 10:40:10 -0400

    na

---
+++ scripts/note.js
@@ -0,0 +1,14 @@
+// entity for rendering html elements "in" the canvas
+import { Entity } from "./entity.js";
+
+export class Note extends Entity {
+    constructor(url) {
+        super(); // call entity class constructor
+
+        // set any note specific properties
+    }
+
+    render(ctx) {
+        // render the note
+    }
+}
